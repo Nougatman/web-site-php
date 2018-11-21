abbf6f631178239e581c9bacf520bc4dc64d1d56
Evgeniy Seleznev <Nightwish18021996@gmail.com>
2018-11-21 11:48:47 +0500

    Add files via upload

---
+++ data/main.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Главная страница сайта</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <h1 align="center">Название сайта</h1>
+  </body>
+</html>
+
+<?php
+    echo "GOOOOOOOOOOD";
+?>
